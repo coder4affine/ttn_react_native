4286153bae143a524089853ce24bf33c2eb40c55
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-01-14 12:11:06 +0530

    commit code

---
+++ ios/Device.h
@@ -0,0 +1,17 @@
+//
+//  Device.h
+//  ttn_react_native
+//
+//  Created by Yagnesh Modh on 13/01/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Device : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Device.m
@@ -0,0 +1,13 @@
+//
+//  Device.m
+//  ttn_react_native
+//
+//  Created by Yagnesh Modh on 13/01/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "Device.h"
+
+@implementation Device
+
+@end
